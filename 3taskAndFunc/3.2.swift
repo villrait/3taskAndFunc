124056a8e81b82aaedd78af4f3b9a8b683fea952
villrait <166745091+villrait@users.noreply.github.com>
2024-07-18 16:37:57 +0500

    1

---
+++ 3taskAndFunc/3.2.swift
@@ -0,0 +1,8 @@
+//
+//  3.2.swift
+//  3taskAndFunc
+//
+//  Created by м on 18.07.2024.
+//
+
+import Foundation
---
+++ 3taskAndFunc/3.3.swift
@@ -0,0 +1,8 @@
+//
+//  3.3.swift
+//  3taskAndFunc
+//
+//  Created by м on 18.07.2024.
+//
+
+import Foundation
